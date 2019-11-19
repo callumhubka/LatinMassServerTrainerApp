6d7ab24c958709457690cb39910bb707f23265cd
callumhubka <51666485+callumhubka@users.noreply.github.com>
2019-11-18 19:35:14 -0600

    Add files via upload

--- LatinMassApp/Latin Mass Server Trainer/Latin Mass Server Trainer/ViewController.swift
+++ LatinMassApp/Latin Mass Server Trainer/Latin Mass Server Trainer/ViewController.swift
@@ -10,10 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //giving buttons and textbox outlets so I can modify them programmatically
+    @IBOutlet var learnButton: UIButton!
+    @IBOutlet var quizButton: UIButton!
+    @IBOutlet var textBox: UITextView!
+    
     //all buttons for this page controlled via view controller rather than programmatically
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //giving buttons and textbox rounded corners
+        learnButton.layer.cornerRadius = 20
+        quizButton.layer.cornerRadius = 20
+        textBox.layer.cornerRadius = 10
+        learnButton.clipsToBounds = true
+        quizButton.clipsToBounds = true
+        textBox.clipsToBounds = true
         
     }
     //unwind segue called by all other pages to go to home page
